e04876409a8848e3357325663a48b707b464801e
juliocaicedo <jcaicedo@linets.cl>
2025-10-01 17:09:58 -0300

    feat: Enhance local development environment by blocking real apply and removing AWS credentials

--- infra/terraform/iam_task_role.tf
+++ infra/terraform/iam_task_role.tf
@@ -49,11 +49,13 @@ data "aws_iam_policy_document" "ecs_tf_network_dev" {
     sid    = "EC2VpcCrudBasic"
     effect = "Allow"
     actions = [
+      # CRUD básico de red
       "ec2:CreateVpc",
       "ec2:DeleteVpc",
       "ec2:ModifyVpcAttribute",
       "ec2:CreateSubnet",
       "ec2:DeleteSubnet",
+      "ec2:ModifySubnetAttribute",
       "ec2:CreateInternetGateway",
       "ec2:DeleteInternetGateway",
       "ec2:AttachInternetGateway",
@@ -67,16 +69,25 @@ data "aws_iam_policy_document" "ecs_tf_network_dev" {
       "ec2:DeleteRoute",
       "ec2:CreateTags",
       "ec2:DeleteTags",
+
+      # Lecturas que Terraform usa en plan/apply
       "ec2:DescribeVpcs",
       "ec2:DescribeSubnets",
       "ec2:DescribeInternetGateways",
       "ec2:DescribeRouteTables",
       "ec2:DescribeAvailabilityZones",
-      "ec2:DescribeTags"
+      "ec2:DescribeTags",
+
+      "ec2:DescribeVpcAttribute",
+      "ec2:DescribeAccountAttributes",
+
+      # útil si luego usas prefix lists (no estorba)
+      "ec2:GetManagedPrefixListEntries"
     ]
     resources = ["*"]
   }
 }
+
 
 resource "aws_iam_policy" "ecs_tf_network_dev" {
   name   = "${var.project}-${var.env}-ecs-tf-network-dev"
